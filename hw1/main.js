fa0bcf63aed4a4a446ffe31f1180d9fd017fd2d1
regvud <silveron.regvud@gmail.com>
2023-05-02 14:19:32 +0300

    first 3 tasks completed

--- hw1/main.js
+++ hw1/main.js
@@ -1,2 +1,25 @@
-console.log('ajdkaswdwq')
-console.log('ajdkaswdwq')
+let greeting = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let nine = -999;
+let onetwothree = 123;
+let pi = 3.14;
+let twoseven = 2.7;
+
+console.log(greeting, owu, com, ua, one, ten, nine, onetwothree, pi, twoseven)
+
+let firstName = 'Іван';
+let middleName = 'Віталійович';
+let lastName = 'Підопригора';
+let person = `${lastName} ${firstName} ${middleName}`;
+
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof (a), typeof (b), typeof (c));
