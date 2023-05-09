2ad7c9be0103ca4cb1f6d4165ba9a81fb3e29c68
regvud <silveron.regvud@gmail.com>
2023-05-09 17:46:28 +0300

    h3_loops corrected

--- hw3_loops/main.js
+++ hw3_loops/main.js
@@ -42,14 +42,14 @@ while (block1 < 20) {
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 
 let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write('<ul>')
 for (const item of listOfItems) {
-    document.write(
-        `<ul>
-            <li>${item}</li>
-        </ul>`)
+    document.write(`<li>${item}</li>`);
+
 
 }
-
+document.write('</ul>')
 
 //     Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
@@ -78,28 +78,23 @@ for (const item of listOfItems) {
 //     },
 // ];
 
-let products = [
-    {
-        title: 'milk',
-        price: 22,
-        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-    },
-    {
-        title: 'juice',
-        price: 27,
-        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-    },
-    {
-        title: 'tomato',
-        price: 47,
-        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-    },
-    {
-        title: 'tea',
-        price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-    },
-];
+let products = [{
+    title: 'milk',
+    price: 22,
+    image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+}, {
+    title: 'juice',
+    price: 27,
+    image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+}, {
+    title: 'tomato',
+    price: 47,
+    image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+}, {
+    title: 'tea',
+    price: 15,
+    image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+},];
 
 // f// ШАБЛОН
 // // <div class="product-card">
@@ -110,12 +105,10 @@ let products = [
 
 for (let i = 0; i < products.length; i++) {
     const product = products[i];
-    document.write(
-        `<div>
+    document.write(`<div>
             <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
             <img src="${product.image}" alt="" class="product-image">
-        </div>`
-    )
+        </div>`)
 }
 
 // --------------------
@@ -139,19 +132,19 @@ for (let i = 0; i < products.length; i++) {
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
 
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+let users = [{name: 'vasya', age: 31, status: false}, {name: 'petya', age: 30, status: true}, {
+    name: 'kolya',
+    age: 29,
+    status: true
+}, {name: 'olya', age: 28, status: false}, {name: 'max', age: 30, status: true}, {
+    name: 'anya',
+    age: 31,
+    status: false
+}, {name: 'oleg', age: 28, status: false}, {name: 'andrey', age: 29, status: true}, {
+    name: 'masha',
+    age: 30,
+    status: true
+}, {name: 'olya', age: 31, status: false}, {name: 'max', age: 31, status: true}];
 console.log('TRUE STATUS');
 for (const user of users) {
     if (user.status) {
