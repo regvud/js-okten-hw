7247d67e692d6734c3dd7160acf1471b754066c9
regvud <silveron.regvud@gmail.com>
2023-05-30 18:10:33 +0300

    stuff

---
+++ new.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        name: 'ivan',
+        price: 100
+    }, {
+        name: 'lesha',
+        price: 200
+    }
+];
+const map = users.map(function ({...value}, index) {
+    value.id = index
+    value.wife = {name: 'lena', skills: ['js', 'python']}
+    return value
+});
+
+
+// console.log(map.filter(value=>value.wife.name === 'lena'))
+
+
+
