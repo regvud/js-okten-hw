793925ee735726c67b21454a677edcf029055d3b
regvud <silveron.regvud@gmail.com>
2023-06-04 13:16:54 +0300

    hw10 done

--- hw10_events/task8/main.js
+++ hw10_events/task8/main.js
@@ -2,3 +2,20 @@
 // при перезавантаженні сторінки до значаення додається по 10грн, але !!!
 //     зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
 //     При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд - нічого не відбувається
+
+const block = document.createElement('h1');
+const time = new Date().getTime();
+let amount = JSON.parse(localStorage.getItem('amount')) || 100;
+
+const storageTime = JSON.parse(localStorage.getItem('time'));
+localStorage.setItem('time', JSON.stringify(time))
+
+block.innerText = `${amount} UAH`;
+document.body.appendChild(block);
+
+console.log(time - storageTime)
+
+if (time - storageTime) {
+    amount += 10;
+    localStorage.setItem('amount', amount);
+}
