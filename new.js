2d940bc580b35b603ef088c0ce6f1fc9721219e0
regvud <silveron.regvud@gmail.com>
2023-06-05 19:17:23 +0300

    stuff

--- new.js
+++ new.js
@@ -1,20 +1,20 @@
-const users = [
-    {
-        name: 'ivan',
-        price: 100
-    }, {
-        name: 'lesha',
-        price: 200
-    }
-];
-const map = users.map(function ({...value}, index) {
-    value.id = index
-    value.wife = {name: 'lena', skills: ['js', 'python']}
-    return value
-});
-
-
-// console.log(map.filter(value=>value.wife.name === 'lena'))
-let number = 5.1
-console.log(Math.floor(number))
-
+// const users = [
+//     {
+//         name: 'ivan',
+//         price: 100
+//     }, {
+//         name: 'lesha',
+//         price: 200
+//     }
+// ];
+// const map = users.map(function ({...value}, index) {
+//     value.id = index
+//     value.wife = {name: 'lena', skills: ['js', 'python']}
+//     return value
+// });
+//
+//
+// // console.log(map.filter(value=>value.wife.name === 'lena'))
+// let number = 5.1
+// console.log(Math.floor(number))
+//
